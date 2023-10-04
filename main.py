98147e98f43630b57ee4b4198826b002947a6d20
Ajayydev <136185490+Ajibuah@users.noreply.github.com>
2023-10-04 12:34:16 +0200

    Please merge
    please merge

--- main.py
+++ main.py
@@ -1,53 +1,83 @@
-# create a to_do list for an individual and his various task or activities
-My_to_do = []
+import os
 
-while True:
-    user_input = input("""1 to create,
-2 to read,
-3 to update,
-4 to delete
-5 to quit.
-""")
+class Blog:
+    def __init__(self):
+        self.posts = []
+        self.post_directory = "blog_posts"
+        os.makedirs(self.post_directory, exist_ok=True)
 
-    if user_input == "1":
-        My_to_do_1 = str(input("Enter a task: "))
-        My_to_do.append(My_to_do_1)
-        print( My_to_do_1)
+    def create_post(self, title, content):
+        post = {"title": title, "content": content}
+        self.posts.append(post)
+        self.save_post_to_file(title, content)
 
-    elif user_input == "2":
-        print("To-Do List:")
-        for index, task in enumerate(My_to_do):
-            print(f"{index}. {task}")
+    def save_post_to_file(self, title, content):
+        post_filename = os.path.join(self.post_directory, f"{title}.txt")
+        with open(post_filename, "w") as file:
+            file.write(content)
 
-    elif user_input == "3":
-        print("To-Do List:")
-        for index, task in enumerate(My_to_do):
-            print(f"{index}. {task}")
+    def read_posts(self):
+        for index, post in enumerate(self.posts):
+            print(f"Post {index + 1}:")
+            print(f"Title: {post['title']}")
+            print(f"Content: {post['content']}")
 
-        index_of_item = int(input("Enter the index of the task to update: "))
-        if 1 <= index_of_item <= len(My_to_do):
-            name_of_item = input("Enter the new task name: ")
-            My_to_do[index_of_item - 1] = name_of_item
-            print("Task updated.")
+    def update_post(self, index, title, content):
+        if 1 <= index <= len(self.posts):
+            post = {"title": title, "content": content}
+            self.posts[index - 1] = post
+            self.save_post_to_file(title, content)
         else:
-            print("Invalid index.")
+            print("Invalid post index.")
 
-    elif user_input == "4":
-        print("To-Do List:")
-        for index, task in enumerate(My_to_do):
-            print(f"{index}. {task}")
-
-        index_of_item = int(input("Enter the index of the task to delete: "))
-        if 1 <= index_of_item < len(My_to_do):
-            removed_task = My_to_do.pop(index_of_item - 1)
-            print(removed_task)
+    def delete_post(self, index):
+        if 1 <= index <= len(self.posts):
+            post = self.posts.pop(index - 1)
+            post_title = post['title']
+            self.delete_post_file(post_title)
         else:
-            print("Invalid index.")
+            print("Invalid post index.")
 
-    elif user_input == "5":
-        break
+    def delete_post_file(self, title):
+        post_filename = os.path.join(self.post_directory, f"{title}.txt")
+        if os.path.exists(post_filename):
+            os.remove(post_filename)
 
-    else:
-        print("Invalid input. Please try again.")
+def main():
+    blog = Blog()
 
+    while True:
+        print("Options:")
+        print("1. Create a new post")
+        print("2. Read all posts")
+        print("3. Update a post")
+        print("4. Delete a post")
+        print("5. Quit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            title = input("Enter the title: ")
+            content = input("Enter the content: ")
+            blog.create_post(title, content)
+            print("Post created!")
+        elif choice == "2":
+            blog.read_posts()
+        elif choice == "3":
+            index = int(input("Enter the post index to update: "))
+            title = input("Enter the new title: ")
+            content = input("Enter the new content: ")
+            blog.update_post(index, title, content)
+            print("Post updated!")
+        elif choice == "4":
+            index = int(input("Enter the post index to delete: "))
+            blog.delete_post(index)
+            print("Post deleted!")
+        elif choice == "5":
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
 
